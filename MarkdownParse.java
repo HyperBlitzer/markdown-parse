1d7ef9d66d5d2354e371b1248a2bf4b997055744
HyperBlitzer <josearreguin1676@gmail.com>
2022-01-26 20:34:51 -0800

    fixing code to make test 2 work

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,8 +15,13 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1){
+                currentIndex = markdown.length();
+            }
+            else{
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            }
         }
         return toReturn;
     }
